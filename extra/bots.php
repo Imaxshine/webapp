07cade87b48d003c02c555c4edd2858a0c3fcfde
Imaxshinee <imaxshine9@gmail.com>
2025-01-18 22:02:32 +0300

    setting error message

---
+++ extra/bots.php
@@ -0,0 +1,45 @@
+<?php
+    if(!$_SERVER['REQUEST_METHOD'] === "GET"){
+        echo "Only GET method is required";
+        exit;
+    }
+    if(isset($_POST['submit']) && isset($_POST['check_time'])){
+        $name = (string) $_POST['name'];
+        $pass = $_POST['password'];
+        $time = time() - $_POST['check_time'];
+        if($time < 5){
+            die("Request fields not for the bots");
+        }else{
+            echo "Welcome {$name} | Your passwords is {$pass}";
+            exit;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <input type="text" name="name" placeholder="Andika jina lako hapa" required><br><br>
+        <input type="password" name="password" placeholder="Weka neno siri" required><br><br>
+        <input type="hidden" name="check_time" value="<?php echo time(); ?>">
+        <input type="submit" name="submit" value="Tuma">
+    </form>
+    <script>
+        // names = ["juma","Ally","bakari","Asha"];
+        // // field = [];
+        // // nameIndex1 = names.indexOf("Ally")
+        // // nameIndex2 = names.indexOf("juma")
+        // // nameIndex3 = names.indexOf("bakari")
+        // // nameIndex4 = names.indexOf("Asha")
+        // // field = field.push(nameIndex1,nameIndex3,nameIndex4,nameIndex2);
+        
+        // names.splice()
+        // console.log(names)
+    </script>
+</body>
+</html>
